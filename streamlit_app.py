798f000aee2da384daf3c50d705c27d8488e60ef
ErickParra <39738722+ErickParra@users.noreply.github.com>
2024-11-19 15:56:53 -0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -54,7 +54,7 @@ SELECT
 FROM [OemDataProvider].[OemParameterExternalView]
 WHERE 
       [EquipmentModel] = '797F' AND 
-      [EquipmentName] = 'C21' AND 
+      [EquipmentName] = 'C17' AND 
       [ParameterName] IN (
           'Parking Brake (797F)', 
           'Cold Mode (797F)', 
