b4e206edf8d87d2afe05b0a576745579c11b0910
ErickParra <39738722+ErickParra@users.noreply.github.com>
2024-11-19 17:35:19 -0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -44,6 +44,11 @@ def load_data(query, conn_str):
 conn_str = f'DRIVER={{ODBC Driver 17 for SQL Server}};SERVER={server};DATABASE={database};UID={username};PWD={password}'
 
 
+# Lista de equipos disponibles
+available_equipments = ['C17', 'C18', 'C19', 'C20', 'C21', 'C22', 'C23','C24', 'C25', 'C36', 'C34', 'C35', 'C36', 'C37', 'C38', 'C39', 'C40', 'C41','C42','C43', 'C45', 'C48']
+
+# Selector de equipo
+selected_equipment = st.selectbox("Seleccione el equipo:", available_equipments, index=0)
 
 # Construcción dinámica de la consulta SQL
 query = f"""
@@ -632,14 +637,6 @@ st.pyplot(fig)
 
 
 
-
-
-
-# Lista de equipos disponibles
-available_equipments = ['C17', 'C18', 'C19', 'C20', 'C21', 'C22', 'C23','C24', 'C25', 'C36', 'C34', 'C35', 'C36', 'C37', 'C38', 'C39', 'C40', 'C41','C42','C43', 'C45', 'C48']
-
-# Selector de equipo
-selected_equipment = st.selectbox("Seleccione el equipo:", available_equipments, index=0)
 
 
 
